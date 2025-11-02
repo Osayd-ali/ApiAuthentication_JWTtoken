8d068163046e3e3d5b80e75c8ab3ccad3dfedd68
Mir Osayd Ali <mali123@depaul.edu>
2025-11-01 23:35:54 -0500

    Added a layer of JWT token security

---
+++ src/test/java/edu/Osayd/min_jwt/MinJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.Osayd.min_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
